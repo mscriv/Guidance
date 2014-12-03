f18e85fe44b492b2da81cbfe239343ddf3f2969f
Michael Scriven <bigscriv03@gmail.com>
2014-12-03 17:15:57 -0500

    Added datepicker

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  location   :string(255)
+#  event_date :string(255)
+#  starts_at  :string(255)
+#  ends_at    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Event < ActiveRecord::Base
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  location   :string(255)
+#  event_date :string(255)
+#  starts_at  :string(255)
+#  ends_at    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
