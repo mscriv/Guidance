d7920244493339338bafd95f6d7cfd1f48c3b364
Michael Scriven <bigscriv03@gmail.com>
2014-12-04 09:28:07 -0500

    Created user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203214132) do
+ActiveRecord::Schema.define(version: 20141204142727) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20141203214132) do
     t.date     "event_date"
     t.time     "starts_at"
     t.time     "ends_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
