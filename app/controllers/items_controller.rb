2bd96a899faeb0df5414ae2758e687f7469a72bc
Michael Scriven <bigscriv03@gmail.com>
2014-12-05 16:52:14 -0500

    Added links to views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,28 @@ class ItemsController < ApplicationController
   def index
     @list = List.find(params[:list_id])
   end
+
+  def new
+    @list = List.find(params[:list_id])
+    @item = @list.items.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.new(item_params)
+    if @item.save
+      flash[:success] = 'Added new item'
+      redirect_to list_items_path
+    else
+      flash[:error] = 'Unable to add item'
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params[:item].permit(:name)
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < InheritedResources::Base
   private
 
     def list_params
-      params.require(:list).permit(:month)
+      params.require(:list).permit(:month, :items)
     end
 end
 
