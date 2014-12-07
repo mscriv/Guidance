4bb714e80ae501322abb09465769804ff26eaf6c
Michael Scriven <bigscriv03@gmail.com>
2014-12-06 22:31:44 -0500

    modified diploma controller

--- app/controllers/diplomas_controller.rb
+++ app/controllers/diplomas_controller.rb
@@ -1,5 +1,31 @@
 class DiplomasController < InheritedResources::Base
 
+
+  def index
+    @requirement = Requirement.find(params[:requirement_id])
+
+  end
+
+  def new
+    @requirement = Requirement.find(params[:requirement_id])
+    @diploma = @requirement.diploma.new
+
+  end
+
+  def create
+    @requirement = Requirement.find(params[:requirement_id])
+    @diploma = @requirement.diploma.new(diploma_params)
+
+    respond_to do |format|
+      if @diploma.save
+        format.html { redirect_to requirement_diplomas_path, notice: 'Diploma was successfully created.' }
+      else
+        format.html { render :new }
+        format.json { render json: @diploma.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
     def diploma_params
